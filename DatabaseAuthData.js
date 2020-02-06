91860958d77a3c37386d7e067368bb3d6f064170
uidumajogu <idumajogu@gmail.com>
2020-02-05 23:33:15 -0500

    Removed folder from repository

--- DatabaseAuthData.js
+++
@@ -1,4 +0,0 @@
-exports.url =
-  "mongodb+srv://admin:JimbdMTsNLmcFjpa@cluster0-qlcxy.mongodb.net/test?retryWrites=true&w=majority";
-
-exports.dataBase = "ChoiceSpacesDB";
--- database-auth-data.js
+++
@@ -1,4 +0,0 @@
-exports.url =
-  "mongodb+srv://uidumajogu:1stLeo@mgdb+@cluster0-jr35e.mongodb.net/test?retryWrites=true&w=majority";
-
-exports.dataBase = "MyWebsiteDB";
